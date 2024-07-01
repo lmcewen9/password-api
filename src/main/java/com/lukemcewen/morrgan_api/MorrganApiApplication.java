56bd93c739139c98646f45c62ef9e709eed3c9ae
lmcewen9 <89426396+lmcewen9@users.noreply.github.com>
2024-06-30 22:30:10 -0400

    hopefully this works

--- src/main/java/com/lukemcewen/morrgan_api/MorrganApiApplication.java
+++ src/main/java/com/lukemcewen/morrgan_api/MorrganApiApplication.java
@@ -2,12 +2,19 @@ package com.lukemcewen.morrgan_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MorrganApiApplication {
+public class MorrganApiApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(MorrganApiApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(MorrganApiApplication.class);
 	}
 
 }
--- src/main/java/com/lukemcewen/morrgan_api/controller/MorrganApiController.java
+++ src/main/java/com/lukemcewen/morrgan_api/controller/MorrganApiController.java
@@ -1,13 +1,13 @@
 package com.lukemcewen.morrgan_api.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import jakarta.servlet.annotation.WebServlet;
 
 @RestController
-@WebServlet("/api")
 public class MorrganApiController {
-    
+
+    @GetMapping("/api")    
     public String getHello(){
         return "Hello World";
     }
