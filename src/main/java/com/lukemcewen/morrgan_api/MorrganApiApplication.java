2f22e9371b298422b09daf1ed26bf95864de54f0
lmcewen9 <89426396+lmcewen9@users.noreply.github.com>
2024-06-30 23:31:43 -0400

    one last thing

--- src/main/java/com/lukemcewen/morrgan_api/MorrganApiApplication.java
+++ src/main/java/com/lukemcewen/morrgan_api/MorrganApiApplication.java
@@ -3,10 +3,10 @@ package com.lukemcewen.morrgan_api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 //import org.springframework.boot.builder.SpringApplicationBuilder;
-//import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MorrganApiApplication {
+public class MorrganApiApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(MorrganApiApplication.class, args);
