368cd461bf48c03f330b3c76456af8e4bdad8a29
lmcewen9 <89426396+lmcewen9@users.noreply.github.com>
2024-06-30 20:37:19 -0400

    inital commit

---
+++ src/main/java/com/lukemcewen/morrgan_api/MorrganApiApplication.java
@@ -0,0 +1,13 @@
+package com.lukemcewen.morrgan_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MorrganApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MorrganApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lukemcewen/morrgan_api/MorrganApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lukemcewen.morrgan_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MorrganApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
