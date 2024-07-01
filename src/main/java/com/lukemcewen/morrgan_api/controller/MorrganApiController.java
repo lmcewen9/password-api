85ea079005c58d6306033959106ca7fdd84aabfa
lmcewen9 <89426396+lmcewen9@users.noreply.github.com>
2024-06-30 20:48:44 -0400

    fixing api test

--- src/main/java/com/lukemcewen/morrgan_api/controller/MorrganApiController.java
+++ src/main/java/com/lukemcewen/morrgan_api/controller/MorrganApiController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MorrganApiController {
     
-    @GetMapping
+    @GetMapping("/api")
     public String getHello(){
         return "Hello World";
     }
