7ed2bf78c7e1ffbf79c41d50e4f61924c5f612f7
lmcewen9 <89426396+lmcewen9@users.noreply.github.com>
2024-06-30 23:22:25 -0400

    removed request mapping

--- src/main/java/com/lukemcewen/morrgan_api/controller/MorrganApiController.java
+++ src/main/java/com/lukemcewen/morrgan_api/controller/MorrganApiController.java
@@ -1,12 +1,10 @@
 package com.lukemcewen.morrgan_api.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
-@RequestMapping
 public class MorrganApiController {
 
     @GetMapping("/")    
