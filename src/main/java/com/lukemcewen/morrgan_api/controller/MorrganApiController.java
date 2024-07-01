0e7a094df0b0c3c844f9ead16bee0884ac93b03b
lmcewen9 <89426396+lmcewen9@users.noreply.github.com>
2024-06-30 20:39:42 -0400

    adding hello world controller

---
+++ src/main/java/com/lukemcewen/morrgan_api/controller/MorrganApiController.java
@@ -0,0 +1,13 @@
+package com.lukemcewen.morrgan_api.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MorrganApiController {
+    
+    @GetMapping
+    public String getHello(){
+        return "Hello World";
+    }
+}
